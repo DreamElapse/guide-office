3cbe2f8acf43fcf0f62944adde33b498253952c9
DreamElapse <gogs@fake.local>
2021-08-03 18:02:24 +0800

    搜索列表页

---
+++ src/pages/search-list/search-list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="search-list">
+    <base-page-header title="搜索借过"> </base-page-header>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  // import * as Helpers from './modules/helpers'
+  // import API from '@api'
+  const PAGE_NAME = 'SEARCH_LIST'
+  const TITLE = '搜索结果'
+
+  export default {
+    name: PAGE_NAME,
+    page: {
+      title: TITLE
+    },
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      // ...Helpers.computed,
+    },
+    methods: {
+      // ...Helpers.methods,
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .search-list
+    width: 100vw
+    height: 100vh
+</style>
